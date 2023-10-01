bb2388624709855e0cd133cdbeb9af51c6698a1d
PiotrZPL <zpl20052020@protonmail.ch>
2023-10-01 15:45:29 +0200

    Added unique keys to iterated elements

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -14,8 +14,8 @@ export default async function Home() {
                 <h1 className="text-3xl font-bold lg:text-5xl mx-5 text-center">The official Customrombay.org device gallery</h1>
                 {/* <p className="text-xl lg:text-2xl mt-8 mx-5 text-center">A place to find all custom ROMs available for your device.</p> */}
                 <h2 className="text-xl font-bold lg:text-3xl pt-8 mx-5 text-center">We already support 703 devices!</h2>
-                <div>{listOfDevices.map((name: string) => (
-                    <DeviceView deviceName={name.replace(".yaml", "")}></DeviceView>
+                <div>{listOfDevices.map((name: string, index: number) => (
+                    <DeviceView key={`a${index}`} deviceName={name.replace(".yaml", "")}></DeviceView>
                 ))}</div>
             </article>
         </main>
