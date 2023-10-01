587c125ce93c22d52a8fbe04e23bf074102d844a
PiotrZPL <zpl20052020@protonmail.ch>
2023-10-01 15:13:06 +0200

    Changed default port and fetching host

--- app/tools/get_device_info.ts
+++ app/tools/get_device_info.ts
@@ -1,5 +1,5 @@
 export async function getDeviceInfo(deviceName: string) {
-    const res = await fetch(`https://new.customrombay.org/api/`, { next: { revalidate: 60 }, headers: 
+    const res = await fetch(`http://localhost:3001/api/`, { next: { revalidate: 60 }, headers: 
     {
       "command" : "getDeviceInfo",
       "devicename" : deviceName,
--- app/tools/get_list_of_devices.ts
+++ app/tools/get_list_of_devices.ts
@@ -1,5 +1,5 @@
 export async function getListOfDevices() {
-    const res = await fetch(`https://new.customrombay.org/api/`, { next: { revalidate: 5 }, headers: 
+    const res = await fetch(`http://localhost:3001/api/`, { next: { revalidate: 5 }, headers: 
     {
       "command" : "getListOfDevices",  
     }
